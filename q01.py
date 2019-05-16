19fbebff7d8312b925a6e69b9be929fd2ed97375
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-16 00:05:49 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -2,4 +2,9 @@
 ## Imprima la suma de la segunda columna.
 ##
 data = open("data.csv","r").readline()
-data
+[row.split(",") for row in data]
+a = 0
+[row[1] for row in data]
+[a+row for row in data]
+a
+_grade_.py
