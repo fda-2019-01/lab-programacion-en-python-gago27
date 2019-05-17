9c72f9ede8eaaf3a4633c37f3baf9fd14b1ef740
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-17 00:17:33 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,15 +1,3 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
-import glob
-filenames = glob.glob("data.csv")
-data = open("data.csv","r").readlines()
-data = [line[:-1] for line in data]
-data = [line.replace("\t", ",") for line in data]
-data = [line.split(",") for line in data]
-data = [line[1] for line in data]
-a = 0
-for row in data:
-  a= a+ int(row)
-print(a)
-__grade__.py
