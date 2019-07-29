0855e8724bc365f989439fc1ee797539c422b941
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-07-29 13:54:28 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,14 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+import glob
+filenames = glob.glob("data.csv")
+data = open("data.csv","r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t", ",") for line in data]
+data = [line.split(",") for line in data]
+data = [line[1] for line in data]
+a = 0
+for row in data:
+  a = a+ int(row)
+print(a)
